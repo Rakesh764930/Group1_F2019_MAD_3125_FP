52b0cfdaa0ae9ad16d21802391e3fc3c1d115433
Rakesh Kumar <Rakeshkumar922152gmail.com>
2019-11-27 16:07:47 -0500

    SPLASH TIME OUT OF  INT TYPE

--- app/src/main/java/com/example/employeepayrollapplication/SplashActivity.java
+++ app/src/main/java/com/example/employeepayrollapplication/SplashActivity.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SplashActivity extends  AppCompatActivity {
+public class SplashActivity  extends  AppCompatActivity {
 
      int SPLASH_TIME_OUT=5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        
+
     }
 }
