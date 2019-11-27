7217f48463b2bcb40fc8583e3bb0ae6a2994afd2
Rakesh Kumar <Rakeshkumar922152gmail.com>
2019-11-27 16:09:26 -0500

    handler's runnable interface implemented

--- app/src/main/java/com/example/employeepayrollapplication/SplashActivity.java
+++ app/src/main/java/com/example/employeepayrollapplication/SplashActivity.java
@@ -3,6 +3,7 @@ package com.example.employeepayrollapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity  extends  AppCompatActivity {
 
@@ -11,6 +12,11 @@ public class SplashActivity  extends  AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                
+            }
+        })
     }
 }
