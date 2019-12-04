a8e1f1d3771b1081b1bc85ebb002d548018788d8
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-12-03 21:49:44 -0500

    commission based class imported

--- app/src/main/java/com/example/employeepayrollapplication/models/CommissionBasedPartTime.java
+++ app/src/main/java/com/example/employeepayrollapplication/models/CommissionBasedPartTime.java
@@ -2,7 +2,7 @@ package com.example.employeepayrollapplication.models;
 
 public class CommissionBasedPartTime extends PartTime {
 
-    private double commissionPerc;
+    private double  commissionPerc;
 
 
 
