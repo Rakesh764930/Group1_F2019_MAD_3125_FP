a55ba8eb9e5ebe7378665a11ae2319cea9ecc9e7
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-12-03 21:56:26 -0500

    day 2 inal commit

--- app/src/main/java/com/example/employeepayrollapplication/models/PartTime.java
+++ app/src/main/java/com/example/employeepayrollapplication/models/PartTime.java
@@ -7,7 +7,7 @@ public class PartTime  extends Employee {
     //V a r i a b l e   D e c l a r a t i o n
 
     private double rate;
-    private float hoursWorked;
+    private float  hoursWorked;
 
     //G e t t e r   a n d   S e t t e r
 
