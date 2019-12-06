1642fe58da4216627b7ac97cd5c40b5cc9fe537b
maan-satveer <koursatveer12@gmail.com>
2019-12-05 20:42:22 -0500

    intern modified

--- app/src/main/java/com/example/employeepayrollapplication/models/Intern.java
+++ app/src/main/java/com/example/employeepayrollapplication/models/Intern.java
@@ -11,6 +11,23 @@ public class Intern extends Employee {
         this.schoolName = schoolName;
     }
 
+    //V a r i a b l e    D e c l a r a t  i o  n
+
+    private String schoolName;
+
+
+
+//G e t t e r   a n d    S e t t e r
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+
 
 
 }
