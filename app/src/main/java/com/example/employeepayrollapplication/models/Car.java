72f260835ed45a8b00e0d879903187039bd77d3c
Rajdeep <chahal.rajdeep03@gmail.com>
2019-12-05 21:04:15 -0500

    Employee fragment

--- app/src/main/java/com/example/employeepayrollapplication/models/Car.java
+++ app/src/main/java/com/example/employeepayrollapplication/models/Car.java
@@ -2,25 +2,26 @@ package com.example.employeepayrollapplication.models;
 
 public class Car extends Vehicle {
 
-    private String carmodelnumber;
+    private String carmodelnum;
 
     public Car(String carmodelnumber,String make, String plate, int mileage) {
         super(make, plate, mileage);
-        this.carmodelnumber = carmodelnumber;
+        this.carmodelnum = carmodelnum;
     }
 
     public String getCarmodelnumber() {
-        return carmodelnumber;
+        return carmodelnum;
     }
 
     public void setCarmodelnumber(String carmodelnumber) {
-        this.carmodelnumber = carmodelnumber;
+        this.carmodelnum = carmodelnum;
     }
 
-    @Override
-    public String printData() {
-
-        return "\nEmployee has Car"+"\nMake : "+this.getMake()+"\nPlate : "+this.getPlate()+"\nCar Model : "+this.getCarmodelnumber()+"\n";
-    }
+//    @Override
+//
+//    public String printData() {
+//
+//        return "\nEmployee has Car"+"\nMake : "+this.getMake()+"\nPlate : "+this.getPlate()+"\nCar Model : "+this.getCarmodelnumber()+"\n";
+//    }
 
 }
---
+++ app/src/main/java/com/example/employeepayrollapplication/models/EmployeeFragment.java
@@ -0,0 +1,87 @@
+package com.example.employeepayrollapplication.models;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class EmployeeFragment extends Fragment
+{
+
+    ArrayList<Employee> employees_list;
+    Context con;
+    RecyclerView recyclerView;
+    CustomAdapterForRecyclerView adapter;
+
+   
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        this.con = inflater.getContext();
+        Log.e("***************","inside onCreateView");
+
+        View v =  inflater.inflate(R.layout.employee_fragment_layout, container, false);
+
+
+        employees_list = Singleton.getObj().getList();
+
+        this.recyclerView = v.findViewById(R.id.recycler_view);
+        adapter = new CustomAdapterForRecyclerView(employees_list, this.con, new CustomAdapterForRecyclerView.SetCustomClickListener() {
+            @Override
+            public void customOnClick(Employee employee) {
+                Toast.makeText(EmployeeFragment.this.con, employee.getName(), Toast.LENGTH_LONG).show();
+                if(employeeDatailsFragment == null) {
+                    employeeDatailsFragment = new EmployeeDatailsFragment();
+                }
+                employeeDatailsFragment.employeeObject(employee);
+                fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frame_layout , employeeDatailsFragment , "");
+                fragmentTransaction.commit();
+                fragmentTransaction.addToBackStack(null);
+
+            }
+        });
+        this.recyclerView.setAdapter(adapter);
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(this.con));
+
+        return v;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+
+        super.onAttach(context);
+    }
+
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+
+        super.onSaveInstanceState(outState);
+
+
+
+    }
+
+
+
+}
