53aa5cc2ffb142c1d6644c974dae4d2c16e78966
macstudent <ajeets110@gmail.com>
2019-12-05 19:59:34 -0500

    added view holder properties

--- app/src/main/java/com/example/employeepayrollapplication/Custom_Adapter/RecyclerViewCustomAdapter.java
+++ app/src/main/java/com/example/employeepayrollapplication/Custom_Adapter/RecyclerViewCustomAdapter.java
@@ -2,9 +2,12 @@ package com.example.employeepayrollapplication.Custom_Adapter;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.employeepayrollapplication.R;
 
 public class RecyclerViewCustomAdapter extends RecyclerView.Adapter {
 
@@ -27,10 +30,17 @@ public class RecyclerViewCustomAdapter extends RecyclerView.Adapter {
 
     class Viewholder extends RecyclerView.ViewHolder{
 
-        
+        TextView name;
+        TextView gender;
+        TextView age;
 
         public Viewholder(@NonNull View itemView) {
             super(itemView);
+
+            name = itemView.findViewById(R.id.emp_name);
+            gender = itemView.findViewById(R.id.emp_type);
+            age = itemView.findViewById(R.id.emp_age);
+
         }
     }
 
