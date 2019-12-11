0957385ad564643e3ca7330dfb5a119b74ce16a7
RajdeepK-03 <chahal.rajdeep03@gmail.com>
2019-12-11 18:43:18 -0500

    full time fragment

--- app/src/main/java/com/example/employeepayrollapplication/fragments/FullTimeFragment.java
+++ app/src/main/java/com/example/employeepayrollapplication/fragments/FullTimeFragment.java
@@ -28,19 +28,6 @@ import com.example.employeepayrollapplication.models.Vehicle;
 
 
 public class FullTimeFragment extends Fragment implements AddEmployeeInterface {
-    TextView txtId;
-    TextView txtName;
-    TextView txtAge;
-    TextView txtDateOfBirth;
-    RadioGroup radioVehicle;
-    TextView txtSalary;
-    TextView txtBonus;
-    Button btnAddFullTime;
-    private OnFragmentInteractionListener mListener;
-
-    public FullTimeFragment() {
-        // Required empty public constructor
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,86 +44,4 @@ public class FullTimeFragment extends Fragment implements AddEmployeeInterface {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        this.txtSalary = view.findViewById(R.id.text_salary);
-        this.txtBonus = view.findViewById(R.id.text_bonus);
-
-
-        this.btnAddFullTime = view.findViewById(R.id.btn_fulltime_employee);
-        this.btnAddFullTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-                if( !txtBonus.getText().toString().isEmpty()
-                        && !txtSalary.getText().toString().isEmpty()
-                        && !txtId.getText().toString().isEmpty()
-                        && !txtName.getText().toString().isEmpty()
-                        &&  !txtAge.getText().toString().isEmpty()) {
-                    int id_int = Integer.parseInt(txtId.getText().toString());
-                    String name_string = txtName.getText().toString();
-                    String birthyear_int = txtDateOfBirth.getText().toString();
-                    int age_int = Integer.parseInt(txtAge.getText().toString().substring(6));
-                    int salary_int = Integer.parseInt(txtSalary.getText().toString());
-                    int bonus_int = Integer.parseInt(txtBonus.getText().toString());
-
-                    Vehicle vehicle_Vehicle = null;
-                    switch (radioVehicle.getCheckedRadioButtonId()) {
-                        case R.id.radio_car:
-                            vehicle_Vehicle = new Car("",""," ");
-                            break;
-                        case R.id.radio_motorCycle:
-                            vehicle_Vehicle = new MotorCycle("",""," ");
-                            break;
-
-                    }
-                    Singleton.getObj().addIntoList(new
-                            FullTime(id_int, name_string, age_int,salary_int, bonus_int, vehicle_Vehicle ));
-                    Log.e("TAG", "full time added");
-                    Toast.makeText(getActivity(), "Employee Added", Toast.LENGTH_LONG).show();
-                    // Intent mIntent = new Intent(FullTimeFragment.this, LoginActivity.class);
-                    //startActivity(mIntent);
-                    txtSalary.setText(null);
-                    txtBonus.setText(null);
-                    txtName.setText(null);
-                    txtAge.setText(null);
-                    txtDateOfBirth.setText("DateOfBirth : YYYY/MM/DD");
-                    radioVehicle.clearCheck();
-                }
-                else
-                {
-                    Toast.makeText(getActivity(), "No field can be empty and unselected" , Toast.LENGTH_LONG).show();
-                }
-            }
-        });
     }
-
-
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    @Override
-    public void defaultViewsAddEmployeeFragment(TextView id, TextView name, TextView age,
-                                                TextView date, RadioGroup vehicle) {
-
-        this.txtId= id;
-        this.txtName= name;
-        this.txtAge = age;
-        this.txtDateOfBirth = date;
-        this.radioVehicle = vehicle;
-    }
-
-
-    public interface OnFragmentInteractionListener {
-        void onFragmentInteraction(Uri uri);
-    }
-}
