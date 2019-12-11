db19f97987630ae2027145a21b5859d4550752c8
RajdeepK-03 <chahal.rajdeep03@gmail.com>
2019-12-11 18:58:43 -0500

    intern fragment

--- app/src/main/java/com/example/employeepayrollapplication/fragments/InternFragment.java
+++ app/src/main/java/com/example/employeepayrollapplication/fragments/InternFragment.java
@@ -36,4 +36,23 @@ public class InternFragment extends Fragment  implements AddEmployeeInterface {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_intern, container, false);
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+
+        this.schoolName = view.findViewById(R.id.text_schoolName);
+        this.internSalary = view.findViewById(R.id.text_internSalary);
+
+        this.addIntern = view.findViewById(R.id.btn_addIntern);
+        this.addIntern.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                if(!schoolName.getText().toString().isEmpty() && !name.getText().toString().isEmpty()
+                        &&  !age.getText().toString().isEmpty())
+                {
+                    
+                }
