f5dc985a647e0cc0b97aecc63cdbebdfe755aa7c
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-12-11 18:30:05 -0500

    date formatting

--- app/src/main/java/com/example/employeepayrollapplication/fragments/DatePickerFragment.java
+++ app/src/main/java/com/example/employeepayrollapplication/fragments/DatePickerFragment.java
@@ -25,4 +25,15 @@ public class DatePickerFragment extends DialogFragment {
             public void onDateSet(DatePicker view, int yyyy, int mm, int dd) {
                 //  Toast.makeText(getContext(),"year "+year+" month "+month+" day "+day, Toast.LENGTH_LONG).show();
 
+                TextView text_dob =  getActivity().findViewById(R.id.text_date_of_birth);
+                //text_age.setText("DateOfBirth : "+year+"/"+month+"/"+day);
+                int age = Calendar.getInstance().get(Calendar.YEAR) - yyyy;
+
+                TextView text_age = getActivity().findViewById(R.id.text_age);
+                text_age.setText("Age : "+age);
+
+                text_dob.setText("DateOfBirth : "+yyyy+"/"+(mm+1)+"/"+dd);
+            }
+        },year,month,day);
+    }
 }
