2a9b6965f3a25aa7c9f5c026288f1b85e6301b70
RajdeepK-03 <chahal.rajdeep03@gmail.com>
2019-12-11 18:36:01 -0500

    working on employee fragment

--- app/src/main/java/com/example/employeepayrollapplication/fragments/EmployeeFragment.java
+++ app/src/main/java/com/example/employeepayrollapplication/fragments/EmployeeFragment.java
@@ -34,32 +34,4 @@ public class EmployeeFragment extends Fragment {
 
 
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        this.con = inflater.getContext();
-        Log.e("TAG","inside onCreateView");
-        View root = inflater.inflate(R.layout.fragment_employees, container, false);
-        employees_list= Singleton.getObj().getList();
-        this.recyclerView = root.findViewById(R.id.rvEmpList);
-        adapter = new RecyclerViewCustomAdapter(employees_list, this.con, new RecyclerViewCustomAdapter.SetCustomClickListener() {
-            @Override
-            public void customOnClick(Employee e) {
-                Toast.makeText(EmployeeFragment.this.con, e.getName(), Toast.LENGTH_LONG).show();
-                if(employeeDetailsFragment == null){
-                    employeeDetailsFragment = new EmployeeDetailsFragment();
-                }
-                employeeDetailsFragment.employeeObject(e);
-                fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-//               // fragmentTransaction.
-                fragmentTransaction.replace(R.id.nav_host_fragment, employeeDetailsFragment, "");
-                fragmentTransaction.commit();
-                //fragmentTransaction.addToBackStack(null);
-            }
-        });
-        this.recyclerView.setAdapter(adapter);
-        this.recyclerView.setLayoutManager(new LinearLayoutManager(this.con));
-        //final TextView textView = root.findViewById(R.id.text_employees);
-
-        return root;
-    }
-}
+  
